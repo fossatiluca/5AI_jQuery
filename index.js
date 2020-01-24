4316abf7822452118f2fe92c04fb244870269863
Fossati Luca <fossati.luca@osdbsesto>
2020-01-24 10:03:24 +0100

    add button and btn.eventListener

---
+++ index.js
@@ -0,0 +1,9 @@
+var btn=document.createElement("button");
+btn.innerHTML("clicca");
+
+var body=document.getElementsByTagName("body")[0];
+body.append(btn);
+
+btn.addEventListener("click", function(){
+    document.getElementById("h1".innerHTML="ciao come stai");
+});
